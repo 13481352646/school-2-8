a4ca687bf07bba68398f3ed19efeb67629643b0f
zhangzzy <1546744276@qq.com>
2023-06-08 21:24:22 +0800

    张紫瑜

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,13 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  base: "school-2-8/",
+
+  build: {
+    outDir: "docs"
+  },
+
   plugins: [
     vue(), 
     vueJsx(),
